046054836ab52862c5dbcd933130439f9ebdc2f0
gilzilberfeld <gil.zilberfeld@gmail.com>
2018-09-29 12:04:25 +0300

    Empty integration test passing

---
+++ integration-testing-spring/src/test/java/atd/spring/testing/configuration/ExchangeControllerConfiguration.java
@@ -0,0 +1,43 @@
+package atd.spring.testing.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import atd.spring.testing.bills.LineItem;
+import atd.spring.testing.exchange.CentralExchange;
+import atd.spring.testing.exchange.RateTextFileLoader;
+import atd.spring.testing.exchange.SettableExchange;
+import atd.spring.testing.rules.CompositeLineItemRule;
+import atd.spring.testing.rules.LineItemRule;
+
+@Configuration
+public class ExchangeControllerConfiguration {
+	
+	@Bean
+	public CompositeLineItemRule compositeLineItemRule() {
+		List<LineItemRule> rules = new ArrayList<>();
+	    LineItemRule factorRule = new LineItemRule() {
+	      public float getFactor(LineItem t) {
+	        return 1.1f;
+	      }
+	    };
+	    rules.add(factorRule);
+	    
+		return new CompositeLineItemRule(rules);
+	};
+
+	@Bean 
+	public RateTextFileLoader rateTextFileLoader() {
+		return new RateTextFileLoader();
+	}
+	
+	@Bean 
+	public SettableExchange settableExchange() {
+		return new CentralExchange(null);
+	}
+
+}
--- integration-testing-spring/src/test/java/atd/spring/testing/integrationtests/SnowExchangeApplicationTests.java
+++ integration-testing-spring/src/test/java/atd/spring/testing/integrationtests/SnowExchangeApplicationTests.java
@@ -3,10 +3,14 @@ package atd.spring.testing.integrationtests;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import atd.spring.testing.configuration.ExchangeControllerConfiguration;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes= {ExchangeControllerConfiguration.class})
 public class SnowExchangeApplicationTests {
 
 	@Test
